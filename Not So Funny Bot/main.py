ed42ffe4c8779fafe99452987298ffb74b3a5704
Derick Bui <derickbui64@gmail.com>
2021-05-18 17:44:45 -0700

    Update main.py

--- Not So Funny Bot/main.py
+++ Not So Funny Bot/main.py
@@ -14,10 +14,14 @@ async def on_message(message): # only triggers when certain message from others 
   if message.author == client.user: # if self (bot)
     return
   
-  if message.content.startswith('$hello'):
+  if message.content.startswith('-hello'): # The greetings command
     greetings = ['Hello!', 'Tally-Ho!', 'HOWDY!']
+    
+    await message.channel.send(greetings[random.randint(0, len(greetings))]) # await allows to wait for message to be received
 
-    # await allows to wait for message to be received
-    await message.channel.send(greetings[random.randint(0, len(greetings))])
+  if message.content.startswith('-joke'): # The "joke" command
+    joke = ['You\'re mom\'s so fat, a wall containing her broke down!', 'You suck!', 'You smell like farts.']
+
+    await message.channel.send(joke[random.randint(0, len(joke))]) # await allows to wait for message to be received
 
 client.run(os.environ['TOKEN']) # Run bot using private token
